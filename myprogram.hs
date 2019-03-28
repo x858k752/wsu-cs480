361a388f2370bcd24dbbf91b6c04a8d226edd052
x858k752 <mikir.mohamed@gmail.com>
2019-03-28 16:27:55 -0500

    My first comment

---
+++ myprogram.hs
@@ -0,0 +1,8 @@
+cd..
+cat <<EOF > myprogram.hs
+cd..
+cat <<EOF > myprogram.hs
+import Data.Char (digitToInt)
+import Data.List (fold')
+binInt :: String -> Int
+binInt = fold' (\acc x -> acc * 2 + digitToInt x)
